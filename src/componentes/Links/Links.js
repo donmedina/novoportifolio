1fb9c880d2566300700fa8a6f9ccd760b5fa1172
Gabriel Medina <medina.rj@live.com>
2023-07-03 19:53:43 -0300

    alteração no link do perfil do github

--- src/componentes/Links/Links.js
+++ src/componentes/Links/Links.js
@@ -4,7 +4,7 @@ import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
 export const Links = () => {
     return (
         <div className={styles.links}>                   
-            <a className={styles.links__git} href='https://twitter.com/donmedinarj' target='__blank'>
+            <a className={styles.links__git} href='https://github.com/donmedina' target='__blank'>
                 <AiFillGithub size={40} style={{marginRight:'12px'}} /> Github</a>
             <a className={styles.links__lk}href='https://www.linkedin.com/in/gmcsrj/' target='__blank'>
                 <AiFillLinkedin size={40} style={{marginRight:'12px'}}/> Linkedin</a>
